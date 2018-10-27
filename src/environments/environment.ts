80df904b83c450cd94bd98ad1da00095f5ba3402
himanshu saraswat <himanshusaraswat@outlook.com>
2018-10-27 05:50:27 +0530

    Change API URI and firebase account

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyA5qqC3_VWzNvREfQvlNUkdTUym_CtKEhQ',
-    authDomain: 'fire-ng-proto.firebaseapp.com',
-    databaseURL: 'https://fire-ng-proto.firebaseio.com',
-    projectId: 'fire-ng-proto',
-    storageBucket: 'fire-ng-proto.appspot.com',
-    messagingSenderId: '407159742468'
+    apiKey: 'AIzaSyCKM1VlODa0nDYzIimBAmv5EREWztL7Ssc',
+    authDomain: 'ironic-objectivist-220216.firebaseapp.com'
   },
-  API_BASE_URI: 'https://sns-api-207407.appspot.com/api',
+  API_BASE_URI: 'https://ironic-objectivist-220216.appspot.com/api',
   API_BASE_URI_LOCAL: 'http://localhost:8083/api',
-  IMAGE_LOCAL: 'http://localhost:8083/api/gallery/image/',
-  IMAGE: 'https://sns-api-207407.appspot.com/api/gallery/image/',
   id: '_id',
   emailId: 'emailId',
   wrongCredentials: 'auth/wrong-password',

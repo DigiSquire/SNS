61f535b037f92f12e5a2b272d4e526b77218ef44
himanshu saraswat <himanshusaraswat@outlook.com>
2018-12-04 08:35:26 +0530

    subdomain active for API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: 'ironic-objectivist-220216.firebaseapp.com',
     storageBucket: "ironic-objectivist-220216.appspot.com",
   },
-  API_BASE_URI: 'https://ironic-objectivist-220216.appspot.com/api',
+  API_BASE_URI: 'https://apiv1.spacesnstories.com/api',
   API_BASE_URI_LOCAL: 'http://localhost:8083/api',
   id: '_id',
   emailId: 'emailId',

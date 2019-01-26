66ff242a88297cbf8cee12429bc450011944f04b
himanshu saraswat <himanshusaraswat@outlook.com>
2019-01-26 22:34:27 +0530

    Disable paste in input fields

--- src/app/core/numbers-only.directive.ts
+++ src/app/core/numbers-only.directive.ts
@@ -11,17 +11,7 @@ export class NumbersOnlyDirective {
   @HostListener('keydown', ['$event']) onKeyDown(event) {
     const e = <KeyboardEvent>event;
     if (this.OnlyNumber) {
-      if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-        // Allow: Ctrl+A
-        (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
-        // Allow: Ctrl+C
-        (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) ||
-        // Allow: Ctrl+V
-        (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) ||
-        // Allow: Ctrl+X
-        (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
-        // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
+      if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1) {
         // let it happen, don't do anything
         return;
       }
